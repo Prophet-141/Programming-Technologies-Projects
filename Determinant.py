e2df130d1a165af86943b5b9fc644161c997df2a
Konstation <k.glaschenko@gmail.com>
2025-10-24 14:19:06 +0300

    Добавлен файл формата .py

---
+++ Determinant.py
@@ -0,0 +1,48 @@
+import random
+def det2(mat):
+    det2 = mat[0][0]*mat[1][1] - mat[0][1] * mat[1][0]
+    return det2
+def det3(mat, n):
+    x = []
+    for k in range(0, n-1):
+        for i in range(k+1, n):
+            v = mat[i][k]/mat[k][k]
+            for j in range(0, n):
+                mat[i][j] -= mat[k][j]*v
+        print("Шаг " + str(k+1) + ":")
+        for d in range(len(mat)):
+            print(mat[d])
+        print()
+    det = 1
+    for l in range(0, n):
+        det = det * mat[l][l]
+    return round(det)
+print("Введите размерность матрицы")
+n = int(input())
+x = []
+stroca = []
+mat = []
+print("Самостоятельный ввод(1) или случайный(2)?")
+vyb = int(input())
+if vyb == 1:
+    for i in range(n):
+        for j in range(n):
+            stroca.append(int(input()))
+        mat.append(stroca)
+        stroca = []
+elif vyb == 2:
+    for i in range(n):
+        for j in range(n):
+            stroca.append(random.randrange(-256, 256, 1))
+        mat.append(stroca)
+        stroca = []
+print("Сама матрица:")
+for i in mat:
+    print(i)
+print()
+if n == 1:
+    print(x[0][0])
+if n == 2:
+    print("det = " + str(det2(mat)))
+if n > 2:
+    print("det = " + str(det3(mat, n)))
