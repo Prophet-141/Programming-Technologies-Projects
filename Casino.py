2be080d2c83ba3f0c37c4c77b3f009393a4ffa27
vm6676237-coder <vm6676237@gmail.com>
2025-10-24 23:31:35 +0300

    Создание программы
    Можно играть

---
+++ Casino.py
@@ -0,0 +1,60 @@
+import random
+
+print('___________________________________')
+stavka = float(input('Кашелек: $'))
+print('Играем? ;)')
+
+while input().capitalize() == 'Играем' and stavka != 0:
+    print('Сделай денежную ставку: ')
+    C = list(map(int, input().split()))
+    C = [round(C[0] / 100 * stavka, 2), round(C[1] / 100 * stavka, 2), round(C[2] / 100 * stavka, 2)]
+    stavka -= (C[0] + C[1] + C[2])
+    print(*C)
+
+    print('Сделай ставку: ')
+    B = list(map(str, input().split()))
+    B[0] = int(B[0])
+    B[1] = B[1].capitalize()
+    B[2] = B[2].capitalize()
+
+    A = []
+    A.append(random.randint(0, 36))
+
+    if A[0] in [32, 19, 21, 25, 34, 27, 36, 30, 23, 5, 16, 1, 14, 9, 18, 7, 12, 3]:
+        A.append('Красный')
+    elif A[0] == 0:
+        A.append('Зеленый')
+    else:
+        A.append('Черный')
+
+    if A[0] % 2 == 0:
+        A.append('Четное')
+    else:
+        A.append('Нечетное')
+
+    print('Главная ставка: ', *A)
+
+    if A[0] == B[0]:
+        C[0] *= 3
+    else:
+        C[0] = 0
+    if B[1] == A[1]:
+        C[1] *= 2
+    else:
+        C[1] = 0
+    if A[2] == B[2]:
+        C[2] *= 2
+    else:
+        C[2] = 0
+    print('___________________________________')
+
+    stavka = round(stavka + C[0] + C[1] + C[2], 2)
+    print('Кашелек: $', stavka, sep='')
+
+    print('Играем? ;)')
+
+print()
+print()
+print()
+print('             ВИДИМО, ВЫ ВСЕ ПРОИГРАЛИ :(')
+print('         ПОЖАЛУйСТА, ПОПОЛНИТЕ СЧЕТ, ЧТОБЫ ИГРАТЬ ДАЛЬШЕ’)
