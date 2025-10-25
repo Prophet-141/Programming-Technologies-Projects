5f7b8b72dec474d8dfb4f3cbc1940367ae65b25c
Konstation <k.glaschenko@gmail.com>
2025-10-25 18:07:54 +0300

    Графические улучшения

--- Determinant.py
+++ Determinant.py
@@ -3,6 +3,7 @@ def det2(mat):
     det2 = mat[0][0]*mat[1][1] - mat[0][1] * mat[1][0]
     return det2
 def det3(mat, n):
+    shag = 0
     for k in range(0, n-1):
         for i in range(k+1, n):
             if mat[k][k]!=0:
@@ -12,18 +13,34 @@ def det3(mat, n):
                 for z in range(k+1,n):
                     if mat[k][z]!=0:
                         prov+=1
+                        butter = z + 1
                         for c in range(0, n):
                             mat[c][k] += mat[c][z]
                         v = mat[i][k]/mat[k][k]
                         z = n
+                if prov != 0:
+                    shag += 1
+                    print("Шаг " + str(shag) + ":")
+                    print("Прибавим к столбику " + str(k+1) + " столбик " + str(butter))
+                    for d in range(len(mat)):
+                        print(mat[d])
                 if prov == 0:
                     return 0
             for j in range(0, n):
                 mat[i][j] -= mat[k][j]*v
-        print("Шаг " + str(k+1) + ":")
-        for d in range(len(mat)):
-            print(mat[d])
-        print()
+            if v != 0 and v != 1:
+                shag+=1
+                print("Шаг " + str(shag) + ":")
+                print("Вычтем из строки " + str(i+1) + " строку " + str(k+1) + " умноженную на " + str(v))
+                for d in range(len(mat)):
+                    print(mat[d])
+            if v == 1:
+                shag+=1
+                print("Шаг " + str(shag) + ":")
+                print("Вычтем из строки " + str(i+1) + " строку " + str(k+1))
+                for d in range(len(mat)):
+                    print(mat[d])
+            print()
     det = 1
     for l in range(0, n):
         det = det * mat[l][l]
@@ -50,6 +67,7 @@ elif vyb == 2:
             stroca.append(random.randrange(-256, 256, 1))
         mat.append(stroca)
         stroca = []
+print()
 print("Сама матрица:")
 for i in mat:
     print(i)
